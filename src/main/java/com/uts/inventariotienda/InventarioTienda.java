90c1d2bb5598d4647fb239a8e4cc22b9ab5e43c3
elAsksito <xpheri035@gmail.com>
2025-03-31 21:22:52 -0500

    Subiendo proyecto

---
+++ src/main/java/com/uts/inventariotienda/InventarioTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uts.inventariotienda;
+
+/**
+ *
+ * @author xpher
+ */
+public class InventarioTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
